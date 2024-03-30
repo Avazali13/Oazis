85a861d3c4067e363c1ebd6ac2e7c05fc23543cb
Avazali13 <avazalima@code.edu.az>
2024-03-30 18:19:59 +0400

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-/* eslint-disable no-unused-vars */
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GloabalStyles from "./styles/GloabalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-
-
-const H1 = styled.h1`
-font-size: 30px;
-font-weight: 600;
-color: yellow;
-`;
-
-
-const StyledApp = styled.main`
-  background-color: orangered;
-  padding: 20px;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
       <GloabalStyles />
-      <StyledApp>
-        <H1>Salam guluu</H1>
-        <Button onClick={()=>alert('Check In')}> 
-          Check In
-        </Button>
-        <Button onClick={()=>alert('Check Out')}> 
-          Check Out
-        </Button>
-        <Input type="number" placeholder="Number of gests"/>
-        <Input type="number" placeholder="Number of gests"/>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />}></Route>
+
+            <Route path="dashboard" element={<Dashboard />}></Route>
+            <Route path="bookings" element={<Bookings />}></Route>
+            <Route path="cabins" element={<Cabins />}></Route>
+            <Route path="users" element={<NewUsers />}></Route>
+            <Route path="settings" element={<Settings />}></Route>
+            <Route path="account" element={<Account />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
